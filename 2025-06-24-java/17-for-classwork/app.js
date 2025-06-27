97c4000648e8fea076fe67109ab9e7c295dcc6d8
Pavel <lyubimov99@gmail.com>
2025-06-27 15:55:17 +0300

    classwork for

---
+++ 2025-06-24-java/17-for-classwork/app.js
@@ -0,0 +1,3 @@
+for(let num = -100; num<=100 ; num+=2 ){
+document.write(`${num} <br>`)   
+}
---
+++ 2025-06-24-java/19-FOR-Classwork2/app.js
@@ -0,0 +1,5 @@
+const min=+prompt('enter a number')
+const max=+prompt('enter a number max')
+for(let count = min ; count <= max ; count++ ){
+document.write(`${count} <br>`)   
+}
