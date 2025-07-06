ce1da63280467616dac31723059837febc5eac18
Pavel <lyubimov99@gmail.com>
2025-07-06 14:05:37 +0300

    classwork

---
+++ 2025-07-06-JS-functions/09-return-classwork/app.js
@@ -0,0 +1,5 @@
+function isPositive(num) {
+    return num >= 0;
+
+}
+console.log(`the number is ${isPositive(+prompt("enter a number")) ? "positive" : "negative"}`)
---
+++ 2025-07-06-JS-functions/10-absolute-return-classwork/app.js
@@ -0,0 +1,6 @@
+function getAbsolete(num){
+    if(num >= o) return num
+    return -num
+}
+const input = +prompt('enter a number')
+console.log(`the absolute value of ${input} is ${getAbsolete(input)}`)
