e416576c41ad3e5b1f3efba4a4cd9be08903f576
Pavel <lyubimov99@gmail.com>
2025-08-17 15:30:22 +0300

    up

--- 2025-08/2025-08-17/07-inheritence/js/app.js
+++ 2025-08/2025-08-17/07-inheritence/js/app.js
@@ -1,4 +1,12 @@
 import Cat from "./cat.js";
-const mitzi = new Cat(4, 5, 8);
-mitzi.sayMiau();
-console.log(`mitzi weight ${mitzi.weight}`);
+import Dog from "./dog.js";
+
+const mitzi = new Cat(4, 8 ,5)
+mitzi.sayMiau()
+mitzi.brestFeed()
+console.log(`mitzi weights ${mitzi.weight}kg`)
+
+
+const lucky = new Dog(24, 'Border collie')
+lucky.bark()
+console.log(`lucky weights ${lucky.weight}kg`)
