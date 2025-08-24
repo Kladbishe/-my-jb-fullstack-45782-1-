55e8022474743cbfe7bd5896ab5316993ef0e49a
Pavel <lyubimov99@gmail.com>
2025-08-24 15:27:34 +0300

    Up

---
+++ 2025-08/2025-08-24/07-state/src/Components/demo/Demo.tsx
@@ -0,0 +1,23 @@
+import { useState, useEffect, useRef } from "react";
+import "./Demo.css";
+
+export default function Demo() {
+  const firstRender = useRef<string>((new Date()).toLocaleTimeString());
+
+  const [now, setNow] = useState<string>(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNow(new Date().toLocaleTimeString())
+    }, 1*1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="Demo">
+      <p>firstRender at: {firstRender.current}</p>
+      <p>time now: {now}</p>
+    </div>
+  );
+}
